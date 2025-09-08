27958a3cab76b609a0819d391844302dfcab6ee1
ThatFlutter <that.phamvan@192.168.1.200>
2025-09-08 22:16:11 +0700

    add json2jsonl

---
+++ tools/json_2_jsonl.py
@@ -0,0 +1,14 @@
+import json
+
+from jsonlines import jsonlines
+
+input_file = '/Users/that.phamvan/my_ws/master/med-score-vi/data/hospital_108/page1.json'
+output_file = '/Users/that.phamvan/my_ws/master/med-score-vi/data/hospital_108/page1.jsonl'
+
+# Load JSON data
+with open(input_file, 'r') as reader:
+    json_data = json.load(reader)
+
+# Convert to JSONL format
+with jsonlines.open(output_file, 'w') as writer:
+    writer.write_all(json_data)
